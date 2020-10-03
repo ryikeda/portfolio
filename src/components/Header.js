00d1ea9e77f56ffe62fe4bb11254799dce8d397c
rafael ubuntu <ryikeda@outlook.com>
2020-10-04 07:13:05 +0900

    created header

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
+import avatar from "../images/avatarSmall.jpg";
+import Typed from "react-typed";
+
+// CSS STYLES
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: "#007ced",
+  },
+  subTitle: {
+    color: "#FFFFFF",
+  },
+  typedContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100vh",
+    textAlign: "center",
+    zIndex: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.typedContainer}>
+      <Grid container justify="center">
+        <Avatar className={classes.avatar} src={avatar} alt="Rafael Ikeda" />
+      </Grid>
+
+      <Typography className={classes.title} variant="h4">
+        <Typed strings={["Rafael Ikeda"]} typeSpeed={40} />
+      </Typography>
+      <br />
+      <Typography className={classes.subTitle} variant="h5">
+        <Typed
+          strings={["Web Design", "Web Development", "MERN Stack"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,64 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Header from "./Header";
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/core/styles";
+
+// CSS STYLES
+const useStyles = makeStyles((theme) => ({
+  particlesCanvas: {
+    position: "absolute",
+  },
+}));
 
 const Home = () => {
+  const classes = useStyles();
+
   return (
     <div>
       <Navbar />
+      <Particles
+        canvasClassName={classes.particlesCanvas}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 1,
+                color: "#007ced",
+              },
+            },
+            size: {
+              value: 8,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 8,
+                size_min: 0.1,
+                sync: true,
+              },
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: true,
+              },
+            },
+          },
+        }}
+      />
+      <Header />
     </div>
   );
 };
