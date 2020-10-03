6a75d49b5296729ff6186897a8262edc26fc4bd7
rafael ubuntu <ryikeda@outlook.com>
2020-10-04 06:18:02 +0900

    added logic and styles to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import MobilRightMenuSlider from "@material-ui/core/Drawer";
+import avatar from "../images/avatarSmall.jpg";
 import {
   AppBar,
   Toolbar,
@@ -10,6 +13,7 @@ import {
   List,
   Typography,
   Box,
+  ListItemIcon,
 } from "@material-ui/core";
 
 import {
@@ -19,18 +23,99 @@ import {
   Apps,
   ContactMail,
 } from "@material-ui/icons";
+
+// CSS STYLES
+const useStyles = makeStyles((theme) => ({
+  menuSliderContainer: {
+    width: 250,
+    background: "#007ced",
+    height: "100%",
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+  listItem: {
+    color: "#f4f4f4",
+  },
+}));
+
+const menuItems = [
+  {
+    listIcon: <Home />,
+    listText: "Home",
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contacts",
+  },
+];
+
 const Navbar = () => {
-  return (
-    <Box component="nav">
-      <AppBar position="static" style={{ background: "#222" }}>
-        <Toolbar>
-          <IconButton>
-            <ArrowBack style={{ color: "#007ced" }} />
-          </IconButton>
-          <Typography variant="h5">Portfolio</Typography>
-        </Toolbar>
-      </AppBar>
+  const [state, setState] = useState({
+    right: false,
+  });
+  const togglerSlider = (slider, open) => () => {
+    setState({ ...state, [slider]: open });
+  };
+
+  const classes = useStyles();
+  const sideList = (slider) => (
+    <Box
+      className={classes.menuSliderContainer}
+      component="div"
+      onClick={togglerSlider(slider, false)}
+    >
+      <Avatar className={classes.avatar} src={avatar} alt="Rafael Ikeda" />
+      <Divider />
+      <List>
+        {menuItems.map((listItem, key) => (
+          <ListItem button key={key}>
+            <ListItemIcon className={classes.listItem}>
+              {listItem.listIcon}
+            </ListItemIcon>
+            <ListItemText
+              className={classes.listItem}
+              primary={listItem.listText}
+            />
+          </ListItem>
+        ))}
+        <ListItem>
+          <ListItemIcon></ListItemIcon>
+        </ListItem>
+      </List>
     </Box>
+  );
+  return (
+    <>
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "#222" }}>
+          <Toolbar>
+            <IconButton onClick={togglerSlider("right", true)}>
+              <ArrowBack style={{ color: "#007ced" }} />
+            </IconButton>
+            <Typography variant="h5">Portfolio</Typography>
+            <MobilRightMenuSlider
+              open={state.right}
+              anchor="right"
+              onClose={togglerSlider("right", false)}
+            >
+              {sideList("right")}
+            </MobilRightMenuSlider>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   );
 };
 
