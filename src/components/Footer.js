d9105bcd69717ddfef97b7d04a5c1ae400af9992
rafael ubuntu <ryikeda@outlook.com>
2020-10-05 08:23:41 +0900

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import {
-  BottomNavigation,
-  BottomNavigationAction,
-  Grid,
-} from "@material-ui/core";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import MailIcon from "@material-ui/icons/Mail";
