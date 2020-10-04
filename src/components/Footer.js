2f4fcde3196e86af6ed574f1da4538042f470fa6
rafael ubuntu <ryikeda@outlook.com>
2020-10-05 08:16:29 +0900

    added more links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
-// import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Grid,
+} from "@material-ui/core";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import MailIcon from "@material-ui/icons/Mail";
 import AccountBoxIcon from "@material-ui/icons/AccountBox";
@@ -9,10 +13,7 @@ import resume from "../assets/rafael_ikeda_resume.pdf";
 
 const useStyles = makeStyles({
   root: {
-    "& MuiBottomNavigationAction-root": {
-      minWidth: 0,
-      maxWidth: "250",
-    },
+    "& .MuiBottomNavigationAction-root": {},
     "& .MuiSvgIcon-root": {
       fill: "#FFFFFF",
       "&:hover": {
@@ -28,7 +29,15 @@ const Footer = () => {
     <BottomNavigation with="auto" style={{ background: "#016BCB" }}>
       <BottomNavigationAction
         className={classes.root}
-        style={{ padding: 0 }}
+        style={{ padding: 0, minWidth: 0 }}
+        icon={<LinkedInIcon />}
+        href="https://www.linkedin.com/in/rafael-ikeda/"
+        target="_data"
+      />
+
+      <BottomNavigationAction
+        className={classes.root}
+        style={{ padding: 0, minWidth: 0 }}
         icon={<GitHubIcon />}
         href="https://github.com/ryikeda"
         target="_data"
@@ -36,14 +45,15 @@ const Footer = () => {
 
       <BottomNavigationAction
         className={classes.root}
-        style={{ padding: 0 }}
+        style={{ padding: 0, minWidth: 0 }}
         icon={<MailIcon />}
         href="mailto:ryikeda@outlook.com"
         target="_data"
       />
+
       <BottomNavigationAction
         className={classes.root}
-        style={{ padding: 0 }}
+        style={{ padding: 0, minWidth: 0 }}
         icon={<AccountBoxIcon />}
         href={resume}
         target="_data"
